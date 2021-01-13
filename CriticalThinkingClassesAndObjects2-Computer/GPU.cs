3fc939624de34bbe655a03086d5c3efac608fa66
Key Admin <mdepeiza@keyconceptskb.com>
2021-01-13 12:23:22 -0500

    Added classes, Computer, CPU, GPU, HardDrive, Motherboard, and RAM. Also added their member variables.

---
+++ CriticalThinkingClassesAndObjects2-Computer/GPU.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingClassesAndObjects2_Computer
+{
+    class GPU
+    {
+        string manufacturer;
+        double effectiveMemory;
+    }
+}
---
+++ CriticalThinkingClassesAndObjects2-Computer/Motherboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingClassesAndObjects2_Computer
+{
+    class Motherboard
+    {
+        string manufacturer;
+        CPU processor;
+        RAM temporaryMemory;
+        HardDrive storage;
+        GPU graphics;
+    }
+}
